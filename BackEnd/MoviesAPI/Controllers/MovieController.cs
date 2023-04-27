fae6f377fb881a9ca6ae107190ae1b1abb39eec8
Gaberjames10 <gaberjames10@gmail.com>
2023-04-26 21:36:23 -0600

    Mission14

---
+++ BackEnd/MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Data;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private MoviesDbContext context;
+
+        public MovieController(MoviesDbContext temp)
+        {
+            context = temp;
+        }
+
+        public IEnumerable<Movie> Get()
+        {
+            return context.Movies
+                .Where(x => x.Edited != "")
+                .OrderBy(x => x.Title)
+                .ToArray();
+        }
+    }
+}
